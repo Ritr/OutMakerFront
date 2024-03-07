6a40c804c527ba5f9fa492818b120f64afd696cd
itzmrnh <mdnh.42@gmail.com>
2024-03-07 10:17:44 +0600

    Shipping Dicount Updaete

--- src/shared/Navbar/NavbarTop.jsx
+++ src/shared/Navbar/NavbarTop.jsx
@@ -3,11 +3,11 @@ import { AiOutlineArrowRight } from "react-icons/ai";
 
 const NavbarTop = () => {
   return (
-    <div className="bg-primary text-white py-3"  id="tip">
+    <div className="bg-primary text-white py-3" id="tip">
       <div className="w-full">
         <div className="w-full flex justify-center items-center">
           <p className="text-xs md:text-sm lg:text-base underline w-full text-center flex justify-center items-center">
-            20% discount. shipping Australia wide
+            30% discount. shipping Australia wide
             <AiOutlineArrowRight className="ml-2" />
           </p>
         </div>
