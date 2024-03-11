a9aa7f73f5b4c1c632e8fdf87752b3192189f06b
石博文 <337634268@qq.com>
2024-03-11 20:34:59 +0800

    删除日志

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy';
 
 export default ({ mode }) => {
 
   return defineConfig({
-    plugins: [react()],
+    plugins: [react(), legacy({
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true
+        }
+      }
+    })],
   });
 }
