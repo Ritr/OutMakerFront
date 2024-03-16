a109f49f98bfccbd08443f62a9db4ba1cc5faef8
石博文 <337634268@qq.com>
2024-03-16 11:16:29 +0800

    增加导航条百分比

--- src/components/Navbar/CollectionItem.jsx
+++ src/components/Navbar/CollectionItem.jsx
@@ -7,123 +7,113 @@ import { FaArrowRight } from "react-icons/fa";
 import useCollections from "../../Hooks/useCollections";
 import ImgBaseUrl from "../ImgBaseUrl/ImgBaseUrl";
 import { Link } from "react-router-dom";
-import Skeleton from "react-loading-skeleton";
 import { motion } from "framer-motion";
-
+import { SwiperSlide } from "swiper/react";
+import "swiper/css";
+import SwiperWrapper from "../SwiperWrapper";
 const CollectionItem = () => {
   const { collections } = useCollections();
-  const [isDataLoaded, setIsDataLoaded] = useState(false);
-  const [infinite, setInfinite] = useState(false);
-  useEffect(() => {
-    if (collections?.length > 0) {
-      setIsDataLoaded(true);
-    }
-  }, [collections]);
-
   const variants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
   };
+  const [progressValue, setProgressValue] = useState(0);
 
   return (
     <>
       <section className=" bg-white py-5 md:p-0">
-        <div className="pb-20 relative stylish">
+        <div className="pb-4 relative stylish">
           <h3 className="text-primary font-medium text-xl mb-7 uppercase">
             Outmaker Collection
           </h3>
-          <Carousel
-            key={collections.length}
-            additionalTransfrom={0}
-            arrows
-            autoPlaySpeed={3000}
-            centerMode={false}
-            className=""
-            containerclassName="container-with-dots"
-            dotListclassName=""
-            draggable
-            focusOnSelect={false}
-            infinite={false}
-            itemclassName=""
-            keyBoardControl
-            minimumTouchDrag={80}
-            pauseOnHover
-            renderArrowsWhenDisabled={false}
-            renderButtonGroupOutside={false}
-            renderDotsOutside={false}
-            responsive={{
-              desktop: {
-                breakpoint: { max: 5000, min: 1024 },
-                items: 3,
+          <SwiperWrapper
+            showNavigation={true}
+            prevClassName="top-24 md:top-32 "
+            nextClassName="top-24 md:top-32  "
+            swiperProps={{
+              loop: false,
+              onProgress: (swiper, progress) => {
+                setProgressValue(Math.ceil(progress * 10) * 10);
               },
-              tablet: {
-                breakpoint: { max: 1024, min: 780 },
-                items: 2,
-              },
-              mobile: {
-                breakpoint: { max: 780, min: 0 },
-                items: 1,
+              breakpoints: {
+                375: {
+                  slidesPerView: 1.5,
+                },
+                768: {
+                  slidesPerView: 2,
+                  spaceBetween: 10,
+                },
+                1024: {
+                  slidesPerView: 4.5,
+                  spaceBetween: 20,
+                },
               },
             }}
-            rewind={false}
-            rewindWithAnimation={false}
-            rtl={false}
-            shouldResetAutoplay
-            showDots={true}
-            sliderclassName=""
-            slidesToSlide={1}
-            swipeable
           >
             {collections?.map((collection) => (
-              <motion.div
-                key={collection?.collection_id}
-                initial="hidden"
-                animate="visible"
-                variants={variants}
-                transition={{ duration: 0.5 }}
-              >
-                <div className=" space-y-[16px] text-primary border border-gray-50 p-3 rounded card-hover">
-                  <Link
-                    to={`collection-product/${collection?.collection_id}/${collection?.collection_name}`}
-                    className="text-xl md:text-2xl lg:text-3xl"
-                  >
-                    <div
-                      className=" w-full flex items-center justify-center"
-                      style={{
-                        height: 0,
-                        paddingBottom: "66.666%",
-                        position: "relative",
-                      }}
+              <SwiperSlide>
+                <motion.div
+                  key={collection?.collection_id}
+                  initial="hidden"
+                  animate="visible"
+                  variants={variants}
+                  transition={{ duration: 0.5 }}
+                >
+                  <div className=" space-y-[16px] text-primary border border-gray-50 p-3 rounded card-hover">
+                    <Link
+                      to={`collection-product/${collection?.collection_id}/${collection?.collection_name}`}
+                      className="text-xl md:text-2xl lg:text-3xl"
                     >
-                      <img
-                        src={ImgBaseUrl(collection?.collection_pic)}
-                        alt="IMAGE"
-                        className="max-h-full h-full max-w-full rounded object-cover"
-                        style={{ position: "absolute", left: 0, top: 0 }}
-                      />
-                    </div>
+                      <div
+                        className=" w-full flex items-center justify-center"
+                        style={{
+                          height: 0,
+                          paddingBottom: "66.666%",
+                          position: "relative",
+                        }}
+                      >
+                        <img
+                          src={ImgBaseUrl(collection?.collection_pic)}
+                          alt="IMAGE"
+                          className="max-h-full h-full max-w-full rounded object-cover"
+                          style={{ position: "absolute", left: 0, top: 0 }}
+                        />
+                      </div>
 
-                    {collection?.collection_name}
+                      {collection?.collection_name}
 
-                    <p className="text-xs pb-6">
-                      {collection?.collection_desc &&
-                        collection.collection_desc
-                          .split(" ")
-                          .slice(0, 20)
-                          .join(" ")}{" "}
-                      ....
-                    </p>
-                  </Link>
-                </div>
-              </motion.div>
+                      <p className="text-xs pb-6">
+                        {collection?.collection_desc &&
+                          collection.collection_desc
+                            .split(" ")
+                            .slice(0, 20)
+                            .join(" ")}{" "}
+                        ....
+                      </p>
+                    </Link>
+                  </div>
+                </motion.div>
+              </SwiperSlide>
             ))}
-          </Carousel>
-
-          <div className="absolute bottom-[5%] left-[15%] bg-[#d5dfff] p-[1px] ml-20 w-[60%] stylish-devider"></div>
+          </SwiperWrapper>
         </div>
-
+        <div className="md:flex justify-between pb-12 gap-4  items-center">
+          <progress
+            className="progress [&::-webkit-progress-value]:bg-primary [&::-moz-progress-bar]:bg-primary flex-1 duration-500 transition-all"
+            value={progressValue}
+            max="100"
+          ></progress>
+          <Link to="/collections">
+            <button className="mt-4 mx-auto md:mt-0 flex items-center gap-2 bg-primary text-white text-lg py-4 px-7 rounded-full hover:bg-white hover:text-primary border border-primary duration-500 transition-all ease-in-out">
+              <span>View All Collections</span>
+              <span>
+                <FaArrowRight />
+              </span>
+            </button>
+          </Link>
+        </div>
         {/* button */}
-        <div className="my-12 flex items-center lg:items-start justify-center lg:justify-end">
+        {/* <div className="my-12 flex items-center lg:items-start justify-center lg:justify-end">
           <Link to="/collections">
             <button className="flex items-center gap-2 bg-primary text-white text-lg py-4 px-7 rounded-full hover:bg-white hover:text-primary border border-primary duration-500 transition-all ease-in-out">
               <span>View All Collections</span>
@@ -132,7 +122,7 @@ const CollectionItem = () => {
               </span>
             </button>
           </Link>
-        </div>
+        </div> */}
       </section>
     </>
   );
--- src/components/SwiperWrapper/index.jsx
+++ src/components/SwiperWrapper/index.jsx
@@ -3,7 +3,13 @@ import { Swiper } from "swiper/react";
 import "swiper/css";
 import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
 
-const SwiperWrapper = ({ children, showNavigation, swiperProps }) => {
+const SwiperWrapper = ({
+  children,
+  showNavigation,
+  swiperProps,
+  prevClassName,
+  nextClassName,
+}) => {
   const sliderRef = useRef(null);
   const handlePrev = useCallback(() => {
     if (!sliderRef.current) return;
@@ -17,27 +23,29 @@ const SwiperWrapper = ({ children, showNavigation, swiperProps }) => {
   return (
     <div className="relative">
       {showNavigation && (
-        <div className="absolute z-10 top-[50%] left-[1rem] -translate-y-[50%]">
-          <button
-            onClick={handlePrev}
-            className="flex items-center justify-center w-[2rem] h-[2rem]  bg-[#626262] bg-opacity-50 text-white rounded-full  font-semibold"
-          >
-            <SlArrowLeft />
-          </button>
-        </div>
+        <button
+          onClick={handlePrev}
+          className={
+            "absolute z-10 top-[50%] left-[1rem] -translate-y-[50%] flex items-center justify-center w-[2rem] h-[2rem]  bg-[#626262] bg-opacity-80 text-white rounded-full  font-semibold " +
+            prevClassName
+          }
+        >
+          <SlArrowLeft />
+        </button>
       )}
       <Swiper ref={sliderRef} loop={true} {...swiperProps} className="mySwiper">
         {children}
       </Swiper>
       {showNavigation && (
-        <div className="absolute z-10 top-[50%] right-[1rem] -translate-y-[50%]">
-          <button
-            onClick={handleNext}
-            className="flex items-center justify-center w-[2rem] h-[2rem]  bg-[#626262] bg-opacity-50 text-white rounded-full  font-semibold"
-          >
-            <SlArrowRight />
-          </button>
-        </div>
+        <button
+          onClick={handleNext}
+          className={
+            "absolute z-10 top-[50%] right-[1rem] -translate-y-[50%] flex items-center justify-center w-[2rem] h-[2rem]  bg-[#626262] bg-opacity-80 text-white rounded-full  font-semibold " +
+            nextClassName
+          }
+        >
+          <SlArrowRight />
+        </button>
       )}
     </div>
   );
